12a192b4f53acbdb01b07126249a64219b1232ff
jamesllanes <39616421+jamesllanes@users.noreply.github.com>
2018-05-26 21:08:09 +0800

    Fixed dbconfig

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -2,7 +2,7 @@
 	/*$connect=mysqli_connect("localhost","root","") or die("Unable to connect");*/
 	
 	$connect=mysqli_init(); //mysqli_ssl_set($connect, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-	mysqli_real_connect($connect, "cyclecodes.mysql.database.azure.com", "unknownmind@ictc-obar", "#P@ssw0rd", "cyclecodes", "3306");
+	mysqli_real_connect($connect, "cyclecodes.mysql.database.azure.com", "unknownmind@cyclecodes", "#P@ssw0rd", "cyclecodes", "3306");
 
 	mysqli_select_db($connect,"cyclecodes");
 	if (!$connect) {
