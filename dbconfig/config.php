0ce7fde11a81dce178d0fda12d0f315e72d6428c
jamesllanes <39616421+jamesllanes@users.noreply.github.com>
2018-05-26 19:41:06 +0800

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,11 @@
 <?php
-	$connect=mysqli_connect("localhost","root","") or die("Unable to connect");
-	mysqli_select_db($connect,"cycle_codes");
+	/*$connect=mysqli_connect("localhost","root","") or die("Unable to connect");*/
+	
+	$connect=mysqli_init(); //mysqli_ssl_set($connect, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+	mysqli_real_connect($connect, "cyclecodes.mysql.database.azure.com", "unknownmind@ictc-obar", "#P@ssw0rd", "cyclecodes", "3306");
+
+	mysqli_select_db($connect,"cyclecodes");
+	if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+	}
 ?>
