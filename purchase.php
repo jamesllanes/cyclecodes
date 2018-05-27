d2da71a3e85ac48ca997a0df440ff18d2d1b9359
Daphne Denise Ramos <daphne_denise_ramos@dlsl.edu.ph>
2018-05-27 15:13:17 +0800

    Edit purchase - sql

--- purchase.php
+++ purchase.php
@@ -57,14 +57,53 @@
 				  </div>
 				</div>
 				<div class="tab">
-				  <button class="tablinks" onclick="#">Sale & Specials</button>
-				  <button class="tablinks" onclick="location.href='../wishlist.php?customer_ID=<?php echo $_SESSION["customer_ID"]?>&w=0'">Wishlist</button>
-				  <button class="tablinks" onclick="#">Careers</button>
-				  <button class="tablinks" onclick="#">About Us</button>
+					<a href="sale_and_specials.php"><button class="tablinks">Sale & Specials</button></a>
+					<?php
+					if(empty($_SESSION['customer_ID']))
+					{
+
+					}
+					elseif(!empty($_SESSION['customer_ID']))
+					{
+					?>
+					<a href='wishlist.php?customer_ID=<?php echo $_SESSION["customer_ID"]?>&w=0'><button class="tablinks">Wishlist</button></a>
+					<?php
+					}
+					?>
+				  <a href="careers.php"><button class="tablinks">Careers</button></a>
+				  <a href="about_us.php"><button class="tablinks">About Us</button></a>
 				</div>
-				<div class="cart_tab">
-					<p>Cart: 0 Items</p>
-				</div>
+				<?php
+					if(empty($_SESSION['customer_ID']))
+					{
+						?>
+						<div class="cart_tab2">
+						</div>
+						<?php
+					}
+					elseif(!empty($_SESSION['customer_ID']))
+					{
+						$count=0;
+						$select_cart="SELECT * FROM cart WHERE customer_ID='$_SESSION[customer_ID]'";
+						$result2=mysqli_query($connect,$select_cart);
+						if(mysqli_num_rows($result2)>0)
+						{
+							while($row=mysqli_fetch_array($result2))
+							{
+								$count=$count+1;
+							}
+						}
+						else
+						{
+							$count=0;
+						}
+						?>
+						<div class="cart_tab">
+						<a href='cart.php?customer_ID=<?php echo $_SESSION["customer_ID"]?>&c=0'><button class="tablinks">My Cart: <?php echo $count ?> Items</button></a>
+						</div>
+						<?php
+					}
+					?>
 			</div>
 		</div>
 		<div id="Middle"> <!--breadcrumb-->
@@ -73,22 +112,21 @@
 			</div>
 		</div>
 		<div id="Center"> <!--category links-->
-			<div id="subcategories_container">
+			<!--<div id="subcategories_container">
 				<div class="categories_header"><h3>Categories</h3></div><br>
 				<div class="subcategories_content">
 					<ul class="sublinks">
-						<li><a href="bikeparts/all_bikeparts.php">All</a></li><br>
-						<li><a href="bikeparts/accessories.php" class="active">Accessories</a></li><br>
-						<li><a href="bikeparts/brakeset.php">Brakeset</a></li><br>
-						<li><a href="bikeparts/cables.php">Cables</a></li><br>
-						<li><a href="bikeparts/chain.php">Chain</a></li><br>
-						<li><a href="bikeparts/cogs.php">Cogs</a></li>
-						<li><a href="bikeparts/crankset.php">Crankset</a></li><br>
-				
+						<li><a href="all_bikeparts.php">All</a></li><br>
+						<li><a href="accessories.php" class="active">Accessories</a></li><br>
+						<li><a href="brakeset.php">Brakeset</a></li><br>
+						<li><a href="cables.php">Cables</a></li><br>
+						<li><a href="chain.php">Chain</a></li><br>
+						<li><a href="cogs.php">Cogs</a></li>
+						<li><a href="crankset.php">Crankset</a></li><br>
 					</ul>
 				</div>
-			</div>
-			<div class="prod_content">
+			</div>-->
+			<div class="prod_content_cart	">
 				<?php
 					$customer_ID=$_REQUEST['customer_ID'];
 					$address=$_POST['address'];
@@ -101,28 +139,9 @@
 						$card_type=$_POST['card_type'];
 						$card_number=$_POST['card_number'];
 					}
-					/*$select_cart2="SELECT * FROM cart WHERE customer_ID = '$customer_ID'";
-					$result_cart2=mysqli_query($connect,$select_cart2);
-					if(mysqli_num_rows($result_cart2)>0)
-					{
-						while($row=mysqli_fetch_array($result_cart2))
-						{
-							$Prod_ID=$row['Prod_ID'];
-							$Prod_Name=$row['Prod_Name'];
-							$Price=$row['Price'];
-							$Quantity=$row['Quantity'];
-							$Total=$row['Total'];
-
-							//insert items to purchase table
-							$insert_purchase="INSERT INTO purchases (Prod_ID, Prod_Name, Price, Quantity, Total, customer_ID) VALUES ('$Prod_ID', '$Prod_Name', '$Price', '$Quantity', '$Total', $customer_ID')";
-							$result_insert=mysqli_query($connect,$insert_purchase);
-						}
-					}*/
-					echo "<table frame=box bordercolor='#8C001A' class='prod_list' style='width:750px; height: 410px;'>";
+					echo "<table frame=box bordercolor='#8C001A' class='prod_list' style='width:910px; height: 358px;'>";
 					echo "<tr><th colspan='6' class='title'>Order Received<br></th></tr>";
-					$insert_purchase="INSERT INTO purchases (Prod_ID, Prod_Name, Price, Quantity, Total, customer_ID) SELECT Prod_ID, Prod_Name, Price, Quantity, Total, customer_ID FROM cart WHERE customer_ID = '$customer_ID'";
-					$result_insert=mysqli_query($connect,$insert_purchase);
-
+//OPEN cart FOR TOTAL IN SUMMARY//
 					$select_total="SELECT * FROM cart WHERE customer_ID='$customer_ID'";
 					$result_total=mysqli_query($connect,$select_total);
 					$Grand_Total=0;
@@ -133,11 +152,12 @@
 							$Grand_Total=$Grand_Total+$row['Total'];
 						}
 					}
+//END OF SQL: OPEN cart FOR TOTAL IN SUMMARY//
 					echo "<tr class='borderbottom1'><td colspan='6'><center><br>Thank you. Your order has been received.</center><br></td></tr>";
 					if(($_SESSION['mode_of_payment'])=='Cash on Delivery')
 					{
-						echo "<tr><td><br>ORDER NO.:</td><td>TOTAL:</td><td>PAYMENT METHOD:</td><td>ADDRESS:</td></tr>";
-						echo "<tr><td><b>001</b></td><td><b>₱".$Grand_Total."</b></td><td><b>".$_SESSION['mode_of_payment']."</b></td><td><b>".$address."</b></td></tr>";
+						echo "<tr><td><br>ORDER NO.:</td><td>TOTAL:</td><td colspan='2'>PAYMENT METHOD:</td><td colspan='2'>ADDRESS:</td></tr>";
+						echo "<tr><td><b>001</b></td><td><b>₱".$Grand_Total."</b></td><td colspan='2'><b>".$_SESSION['mode_of_payment']."</b></td><td colspan='2'><b>".$address."</b></td></tr>";
 						echo "<tr class='borderbottom1'><td colspan='6'><br><center>Pay with cash upon delivery.</center><br></td></tr>";
 					}
 					else
@@ -148,31 +168,35 @@
 						<td><b>".$address."</b></td></tr>";
 						echo "<tr class='borderbottom1'><td colspan='6'><br><center>Your card transaction is accepted.</center><br></td></tr>";
 					}
-
-					$select_purchase="SELECT * FROM cart WHERE customer_ID='$customer_ID'";
-					$result_purchase=mysqli_query($connect,$select_purchase);
+//ORDER DETAILS: OPEN cart TO DISPLAY//
+					$select_cart="SELECT * FROM cart WHERE customer_ID='$customer_ID'";
+					$result_cart=mysqli_query($connect,$select_cart);
 					$Grand_Total=0;
 					echo "<tr class='title'><td colspan='6'><b>Order Details:</b></td></tr>";
 					echo "<tr class='title'><td colspan='3'><br>Product Name</td><td>Price</td><td>Quantity</td><td>Subtotal</td></tr>";
 					
 
-					if(mysqli_num_rows($result_purchase)>0)
+					if(mysqli_num_rows($result_cart)>0)
 					{
-						while($row=mysqli_fetch_array($result_purchase))
+						while($row=mysqli_fetch_array($result_cart))
 						{
 							$Prod_ID1=$row['Prod_ID'];
 							$_SESSION['Prod_ID1']=$Prod_ID1;
 							$Prod_Name=$row['Prod_Name'];
 							$Price=$row['Price'];
 							$Quantity_update=$row['Quantity'];
+							$Total=$row['Total'];
 							$Subtotal=$Price*$Quantity_update;
 							$Grand_Total=$Grand_Total+$row['Total'];
 
-							
+							$select_purchase="INSERT INTO purchases VALUES ('', '$Prod_ID1', '$Prod_Name', '$Price', '$Quantity_update', '$Total', '$customer_ID')";
+							$result_purchase=mysqli_query($connect,$select_purchase);
+
 							echo "<tr class='borderbottom1'><td colspan='3'>".$Prod_Name."</td><td>₱".$Price."</td><td>x".$Quantity_update."</td><td>₱".$Subtotal."</td></tr>";
 
-							$select_sales="SELECT * FROM sales";
+							$select_sales="SELECT * FROM sales WHERE Prod_ID='$Prod_ID1'";
 							$result_sales=mysqli_query($connect,$select_sales);
+//CONDITION: IF PRODUCT IS ALREADY IN SALES TABLE: UPDATE//
 							if(mysqli_num_rows($result_sales)>0)
 							{
 								while($row=mysqli_fetch_array($result_sales))
@@ -181,43 +205,23 @@
 									if($Prod_ID1==$Prod_ID2)
 									{
 										$Quantity=$row['Quantity']+$Quantity_update;
-										$update_qty="UPDATE sales SET Quantity ='$Quantity'";
+										$update_qty="UPDATE sales SET Quantity ='$Quantity' WHERE Prod_ID='$Prod_ID1'";
 										$result_qty=mysqli_query($connect,$update_qty);
 
 										$Additional=$row['Total']+$Subtotal;
-										$update_total="UPDATE sales SET Total ='$Additional'";
+										$update_total="UPDATE sales SET Total ='$Additional' WHERE Prod_ID='$Prod_ID1'";
 										$result_total=mysqli_query($connect,$update_total);
-
-										//echo "update";
 									}
 								}
 							}
+//CONDITION: IF PRODUCT IS NOT YET IN SALES TABLE: INSERT//
 							else
 							{
-								/*$select_purchase2="SELECT * FROM cart WHERE customer_ID='$customer_ID'";
-								$result_purchase2=mysqli_query($connect,$select_purchase2);
-								if(mysqli_num_rows($result_purchase2)>0)
-								{
-									while($row=mysqli_fetch_array($result_purchase2))
-									{
-										$Prod_ID1=$row['Prod_ID'];
-										$Prod_Name=$row['Prod_Name'];
-										$Price=$row['Price'];
-										$Quantity_update=$row['Quantity'];
-										$Subtotal=$Price*$Quantity_update;
-
-										*/
-										//$insert_sales="INSERT INTO sales (Prod_ID, Prod_Name, Price, Quantity, Total) VALUES ('$Prod_ID1', '$Prod_Name', '$Price', '$Quantity_update', '$Subtotal') WHERE Prod_ID = '$Prod_ID1'";
-										$insert_sales="INSERT INTO sales (Prod_ID, Prod_Name, Price, Quantity, Total) SELECT Prod_ID, Prod_Name, Price, Quantity, Total FROM  cart WHERE customer_ID = '$customer_ID'";
-										$result_insert2=mysqli_query($connect,$insert_sales);
-										//echo "insert sales";
-										//print_r($customer_ID);
-									//}
-								//}
+								$insert_sales="INSERT INTO sales VALUES ('', '$Prod_ID1', '$Prod_Name', '$Price', '$Quantity_update', '$Total')";
+								$result_insert2=mysqli_query($connect,$insert_sales);
 							}
 
 							$select_inv="SELECT * FROM inventory WHERE Prod_ID = '$Prod_ID1'";
-							//print_r($_SESSION['Prod_ID1']);
 							$result_inv=mysqli_query($connect,$select_inv);
 							if(mysqli_num_rows($result_inv)>0)
 							{
@@ -228,6 +232,7 @@
 									$result_update=mysqli_query($connect,$update_inventory);
 								}
 							}
+
 						}
 						echo "<tr class='borderbottom1'><td colspan='5' align= 'center'><br><b>Total</b></td><td><b>₱".$Grand_Total."</b></td></tr>";
 						echo "<tr align='center'><td colspan='6'><br></td></tr>";
@@ -240,8 +245,8 @@
 						?><tr class='no_border'><td colspan='4' align='center'>Cart is empty.</td></tr><?php
 					}	
 
-					//$delete_cart="DELETE FROM cart WHERE customer_ID = '$customer_ID'";
-					//$result_delete=mysqli_query($connect,$delete_cart);
+					$delete_cart="DELETE FROM cart WHERE customer_ID = '$customer_ID'";
+					$result_delete=mysqli_query($connect,$delete_cart);
 				?>
 			</div>
 		</div>
